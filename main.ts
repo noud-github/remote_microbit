de159489df39d4e79d2d0f3cafdcb6c3dc5de7a3
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2023-02-13 20:25:34 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "right") {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (receivedString == "left") {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # . #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
